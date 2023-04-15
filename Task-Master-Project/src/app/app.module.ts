d467acbe4eaaf71a2f07ad57c4539442cd021820
Malia-K <98634931+Malia-K@users.noreply.github.com>
2023-04-16 00:35:53 +0600

    -app-component -> main-page and sign in page

--- Task-Master-Project/src/app/app.module.ts
+++ Task-Master-Project/src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MainPageComponent } from './main-page/main-page.component';
+import { SignInComponent } from './sign-in/sign-in.component';
+import { SignUpComponent } from './sign-up/sign-up.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainPageComponent,
+    SignInComponent,
+    SignUpComponent
   ],
   imports: [
     BrowserModule,
