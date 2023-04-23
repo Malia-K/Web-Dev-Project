b5f28c43c65160b0317f7c8024055115dfaac6bc
Malia-K <98634931+Malia-K@users.noreply.github.com>
2023-04-23 18:44:22 +0600

    +routing between components

--- Task-Master-Project/src/app/app-routing.module.ts
+++ Task-Master-Project/src/app/app-routing.module.ts
@@ -1,7 +1,20 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { SignInComponent } from './sign-in/sign-in.component';
+import { SignUpComponent } from './sign-up/sign-up.component';
+import { MainPageComponent } from './main-page/main-page.component';
+import { PlannerComponent } from './planner/planner.component';
+import { HabitTrackerComponent } from './habit-tracker/habit-tracker.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: "sign-in", component: SignInComponent},
+  {path: "sign-up", component: SignUpComponent},
+  {path: "main-page", component: MainPageComponent},
+  // {path: "main-page/to-do", component: MainPageComponent},
+  // {path: "main-page/planner", component: PlannerComponent},
+  // {path: "main-page/habit-tracker", component:HabitTrackerComponent},
+  {path: "", redirectTo:"sign-in", pathMatch: "full"}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
