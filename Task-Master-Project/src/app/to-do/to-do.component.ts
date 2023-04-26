8375e67b6cc5a29a3a1eecaf7e91d7e49fc96c78
Malia-K <98634931+Malia-K@users.noreply.github.com>
2023-04-26 15:41:29 +0600

    +some css changes

--- Task-Master-Project/src/app/to-do/to-do.component.ts
+++ Task-Master-Project/src/app/to-do/to-do.component.ts
@@ -8,11 +8,13 @@ import { Category, Todo } from './models/models';
 })
 export class ToDoComponent implements OnInit{
   categories: Category[] = [
-    {id: 1, title: "Webka"}, {id:2, title: "baska sabak"}, {id: 3, title:"3 category"},{id: 3, title:"3 category"}  
+    {id: 1, title: "Webka"}, {id:2, title: "baska sabak"}, {id: 3, title:"3 category"},{id: 4, title:"3 category"}  
   ];
   todos: Todo[] = [
-    {id:1, description:"something to do", completed:false, category:1},
-    {id:3, description:"something to do", completed:false, category:3},
+    {id:1, description:"something to dokfhnvkushfvhsfkvhkhvkjhfvkhvkhh   webka", completed:false, category:1},
+    {id:1, description:"something to dokfhnvkushfvhsfkvhkhvkjhfvkhvkhh   webka", completed:false, category:1},
+    {id:1, description:"something to dokfhnvkushfvhsfkvhkhvkjhfvkhvkhh   webka", completed:false, category:1},
+    {id:3, description:"something to do 3 cat", completed:false, category:3},
   ];
 
 
